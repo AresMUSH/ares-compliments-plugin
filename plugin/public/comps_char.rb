b9fdff70833f57a4f4ecdc49093e330fab665458
Nicole <102542459+aerrin99@users.noreply.github.com>
2023-05-11 00:01:49 -0400

    Move the comps model into the aresmush module properly

--- plugin/public/comps_char.rb
+++ plugin/public/comps_char.rb
@@ -4,13 +4,13 @@ module AresMUSH
     collection :comps, "AresMUSH::Comps"
     attribute :comps_given, :type => DataType::Integer
   end
-end
 
+  class Comps < Ohm::Model
+    include ObjectModel
 
-class Comps < Ohm::Model
-  include ObjectModel
-
-  reference :character, "AresMUSH::Character"
-  attribute :comp_msg
-  attribute :from
+    reference :character, "AresMUSH::Character"
+    attribute :comp_msg
+    attribute :from
+  end
+  
 end
